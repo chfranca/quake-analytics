9e5e72b0899b24f382b4969c0999c8cc1cc9da7d
Carlos Franca <chenriqueinfo@gmail.com>
2024-03-11 11:38:06 -0300

    extracts the log filename to args

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	analytics "quake-logs-parser/analytics"
 )
 
 func main() {
 
-	file := "qgames.log"
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		log.Fatal("You must inform a filepath as argument the program")
+		return
+	}
+
+	filepath := args[0]
+
 	report := analytics.Report{Games: []analytics.Game{}}
-	analytics.ProcessLog(file, &report)
+	analytics.ProcessLog(filepath, &report)
 
 	for i, v := range report.Games {
 		f := fmt.Sprintf(`{"game_%d": %s}`, i+1, v)
